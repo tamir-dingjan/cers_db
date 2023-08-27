dceef03804c6c0889ae8375d54dca603f57a90b8
Tamir Dingjan <tamir.dingjan@gmail.com>
2023-08-27 12:09:27 +0300

    add file upload route

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'xlsx', 'xls'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///text_db.sqlite3'
 db = SQLAlchemy(app)
 
@@ -9,6 +15,12 @@ class TextEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     text = db.Column(db.String(500), nullable=False)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,6 +34,25 @@ def index():
     else:
         text_entries = TextEntry.query.all()
         return render_template('index.html', text_entries=text_entries)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the request has a file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # Catch empty filenames
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+
 
 if __name__ == '__main__':
     with app.app_context():
